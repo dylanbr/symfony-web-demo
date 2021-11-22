cad6015774f88136dc96d2031aa53a1be24357db
Dylan Bridgman <light@ether.org.za>
2021-11-22 17:29:42 +0200

    Initial import of application

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+		$error = $authenticationUtils->getLastAuthenticationError();
+		$lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+			'last_username' => $lastUsername,
+			'error' => $error
+        ]);
+    }
+
+	public function logout(): void
+	{
+		// empty function, handled by config/packages/security.yml firewall setting
+	}
+}
